ae2a7952701db5d83c9f80ae2084ca51d8fde574
Brian Rojas Chale <Brarojasc@gmail.com>
2019-10-14 02:07:35 -0600

    installer

--- installer.sh
+++ installer.sh
@@ -1,1 +1,17 @@
 #! /bin/bash
+
+args=("$@")
+alias=${args[0]}
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd ~/ ; mkdir scripts;
+cp $SCRIPTPATH/scripts/vgserver.sh ~/scripts/
+
+if [[ ! -z "$alias" ]]
+then
+    sudo echo "alias $alias='~/scripts/vgserver.sh'" >> ~/.bashrc
+else
+    sudo echo "alias vgserver='~/scripts/vgserver.sh'" >> ~/.bashrc
+fi
+cd ~/
+source ~/.bashrc
--- scripts/bash-aliases.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-sudo echo "alias sshvg='ssh vagrant@localhost -p 2222'" >> ~/.bashrc
